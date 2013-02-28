cc4619a6b969c33bd4f310cfdf6601ed1fe2d836
Hasan Karahan <hasan.karahan81@gmail.com>
2013-02-28 19:57:26 +0500

    minor

--- typo/typo.js
+++ typo/typo.js
@@ -27,9 +27,9 @@ var Typo = function (lingua, aff_data, dic_data, settings) {
     this.rules = this.aff_parse(aff_data);
     this.CRCs = {};
 
-    for (var i = 0, _len = this.CRs.length; i < _len; i++) {
+    for (var i=0, _len=this.CRs.length; i<_len; i++) {
         var rule = this.CRs[i];
-        for (var j = 0, _jlen = rule.length; j < _jlen; j++) {
+        for (var j=0, _jlen=rule.length; j<_jlen; j++) {
             this.CRCs[rule[j]] = [];
         }
     }
@@ -51,11 +51,12 @@ var Typo = function (lingua, aff_data, dic_data, settings) {
     // Build the full regular expressions for each compound rule. I've a
     // feeling (but no confirmation yet) that this method of testing for
     // compound words is probably *slow*!
-    for (var i = 0, _len = this.CRs.length; i < _len; i++) {
+    for (var i=0, _len=this.CRs.length; i<_len; i++) {
+
         var rule_text = this.CRs[i];
         var expr_text = "";
 
-        for (var j = 0, _jlen = rule_text.length; j < _jlen; j++) {
+        for (var j=0, _jlen=rule_text.length; j<_jlen; j++) {
             var ch = rule_text[j];
             if (ch in this.CRCs)
                 expr_text += "(" + this.CRCs[ch].join("|") + ")";
@@ -75,8 +76,8 @@ var Typo = function (lingua, aff_data, dic_data, settings) {
 Typo.prototype = {
 
     load: function (object) {
-        for (var key in object) if (object.hasOwnProperty (key)) {
-            this[key] = object[key];
+        for (var key in object) {
+            if (object.hasOwnProperty (key)) this[key] = object[key];
         } return this;
     },
 
@@ -95,18 +96,15 @@ Typo.prototype = {
     ///////////////////////////////////////////////////////////////////////////
 
     aff_parse: function (data) {
+
         var rules = {};
-
-        // Remove comment lines
         data = this.aff_clean(data);
-
         var lines = data.split("\n");
 
         for (var i = 0, _len = lines.length; i < _len; i++) {
+
             var line = lines[i];
-
             var definitionParts = line.split(/\s+/);
-
             var ruleType = definitionParts[0];
 
             if (ruleType == "PFX" || ruleType == "SFX") {
@@ -116,40 +114,35 @@ Typo.prototype = {
 
                 var entries = [];
 
-                for (var j = i + 1, _jlen = i + 1 + numEntries; j < _jlen; j++) {
-                    var line = lines[j];
+                for (var j=i+1, _jlen=i+1+numEntries; j<_jlen; j++) {
 
+                    var line = lines[j];
                     var lineParts = line.split(/\s+/);
                     var charactersToRemove = lineParts[2];
-
                     var additionParts = lineParts[3].split("/");
-
                     var charactersToAdd = additionParts[0];
                     if (charactersToAdd === "0") charactersToAdd = "";
 
-                    var continuationClasses = this.parse_rcs(additionParts[1]);
-
+                    var cont_cls = this.parse_rcs(additionParts[1]);
                     var regexToMatch = lineParts[4];
+                    var entry = {add: charactersToAdd};
 
-                    var entry = {};
-                    entry.add = charactersToAdd;
-
-                    if (continuationClasses.length > 0) entry.continuationClasses = continuationClasses;
+                    if (cont_cls.length > 0) {
+                        entry.continuationClasses = cont_cls;
+                    }
 
                     if (regexToMatch !== ".") {
                         if (ruleType === "SFX") {
                             entry.match = new RegExp(regexToMatch + "$");
-                        }
-                        else {
+                        } else {
                             entry.match = new RegExp("^" + regexToMatch);
                         }
                     }
 
                     if (charactersToRemove != "0") {
                         if (ruleType === "SFX") {
-                            entry.remove = new RegExp(charactersToRemove + "$");
-                        }
-                        else {
+                            entry.remove = new RegExp(charactersToRemove+"$");
+                        } else {
                             entry.remove = charactersToRemove;
                         }
                     }
@@ -157,36 +150,35 @@ Typo.prototype = {
                     entries.push(entry);
                 }
 
-                rules[ruleCode] = { "type": ruleType, "combineable": (combineable == "Y"), "entries": entries };
+                rules[ruleCode] = {
+                    "type": ruleType,
+                    "combineable": (combineable == "Y"),
+                    "entries": entries
+                };
 
                 i += numEntries;
-            }
-            else if (ruleType === "COMPOUNDRULE") {
+            } else
+
+            if (ruleType === "COMPOUNDRULE") {
                 var numEntries = parseInt(definitionParts[1], 10);
 
-                for (var j = i + 1, _jlen = i + 1 + numEntries; j < _jlen; j++) {
+                for (var j=i+1, _jlen=i+1+numEntries; j<_jlen; j++) {
                     var line = lines[j];
-
                     var lineParts = line.split(/\s+/);
                     this.CRs.push(lineParts[1]);
                 }
 
                 i += numEntries;
-            }
-            else if (ruleType === "REP") {
-                var lineParts = line.split(/\s+/);
+            } else
 
+            if (ruleType === "REP") {
+                var lineParts = line.split(/\s+/);
                 if (lineParts.length === 3) {
                     this.ersatz_tbl.push([ lineParts[1], lineParts[2] ]);
                 }
             }
-            else {
-                // ONLYINCOMPOUND
-                // COMPOUNDMIN
-                // FLAG
-                // KEEPCASE
-                // NEEDAFFIX
 
+            else { // ONLYINCOMPOUND, COMPOUNDMIN, FLAG, KEEPCASE, NEEDAFFIX
                 this.flags[ruleType] = definitionParts[1];
             }
         }
@@ -230,31 +222,33 @@ Typo.prototype = {
                 var ruleCodesArray = this.parse_rcs(parts[1]);
 
                 // Save the ruleCodes for compound word situations.
-                if (!("NEEDAFFIX" in this.flags) || ruleCodesArray.indexOf(this.flags.NEEDAFFIX) == -1) {
+                if (!("NEEDAFFIX" in this.flags) ||
+                    ruleCodesArray.indexOf(this.flags.NEEDAFFIX) == -1) {
                     dictionaryTable[word] = ruleCodesArray;
                 }
 
-                for (var j = 0, _jlen = ruleCodesArray.length; j < _jlen; j++) {
+                for (var j=0, _jlen=ruleCodesArray.length; j<_jlen; j++) {
+
                     var code = ruleCodesArray[j];
-
                     var rule = this.rules[code];
-
                     if (rule) {
+
                         var newWords = this.apply_rule(word, rule);
 
-                        for (var ii = 0, _iilen = newWords.length; ii < _iilen; ii++) {
-                            var newWord = newWords[ii];
+                        for (var ii=0, _iilen=newWords.length; ii<_iilen; ii++) {
 
+                            var newWord = newWords[ii];
                             dictionaryTable[newWord] = "";
 
                             if (rule.combineable) {
                                 for (var k = j + 1; k < _jlen; k++) {
+
                                     var combineCode = ruleCodesArray[k];
-
                                     var combineRule = this.rules[combineCode];
-
                                     if (combineRule) {
-                                        if (combineRule.combineable && (rule.type != combineRule.type)) {
+                                        if (combineRule.combineable &&
+                                            (rule.type != combineRule.type)) {
+
                                             var otherNewWords = this.apply_rule(newWord, combineRule);
 
                                             for (var iii = 0, _iiilen = otherNewWords.length; iii < _iiilen; iii++) {
@@ -272,8 +266,7 @@ Typo.prototype = {
                         this.CRCs[code].push(word);
                     }
                 }
-            }
-            else {
+            } else {
                 dictionaryTable[word] = "";
             }
         }
@@ -311,15 +304,14 @@ Typo.prototype = {
 
                 if (rule.type === "SFX") {
                     newWord = newWord + entry.add;
-                }
-                else {
+                } else {
                     newWord = entry.add + newWord;
                 }
 
                 newWords.push(newWord);
 
                 if ("continuationClasses" in entry) {
-                    for (var j = 0, _jlen = entry.continuationClasses.length; j < _jlen; j++) {
+                    for (var j=0, _jlen=entry.continuationClasses.length; j<_jlen; j++) {
                         var continuationRule = this.rules[entry.continuationClasses[j]];
                         if (continuationRule) {
                             newWords = newWords.concat(this.apply_rule(newWord, continuationRule));
@@ -338,19 +330,14 @@ Typo.prototype = {
     parse_rcs: function (text_codes) {
         if (!text_codes) {
             return [];
-        }
-        else if (!("FLAG" in this.flags)) {
+        } else if (!("FLAG" in this.flags)) {
             return text_codes.split("");
-        }
-        else if (this.flags.FLAG === "long") {
+        } else if (this.flags.FLAG === "long") {
             var flags = [];
-            for (var i = 0, _len = text_codes.length; i < _len; i += 2) {
+            for (var i=0, _len=text_codes.length; i<_len; i += 2) {
                 flags.push(text_codes.substr(i, 2));
-            }
-
-            return flags;
-        }
-        else if (this.flags.FLAG === "num") {
+            } return flags;
+        } else if (this.flags.FLAG === "num") {
             return text_codes.split(",");
         }
     },
@@ -359,7 +346,6 @@ Typo.prototype = {
     ///////////////////////////////////////////////////////////////////////////
 
     check: function (word) {
-
         var trimmed = word.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
         if (this.check_exact(trimmed)) return true;
 
@@ -379,7 +365,6 @@ Typo.prototype = {
     },
 
     check_exact: function (word) {
-
         var rule_codes = this.lookup_tbl[word];
         if (typeof rule_codes === 'undefined') {
             return this.compound_min (word);
@@ -390,7 +375,8 @@ Typo.prototype = {
 
     compound_min: function (word) {
 
-        if ("COMPOUNDMIN" in this.flags && word.length >= this.flags.COMPOUNDMIN) {
+        if ("COMPOUNDMIN" in this.flags &&
+            word.length >= this.flags.COMPOUNDMIN) {
             for (var i = 0, _len = this.CRs.length; i < _len; i++) {
                 if (word.match(this.CRs[i])) return true;
             }
